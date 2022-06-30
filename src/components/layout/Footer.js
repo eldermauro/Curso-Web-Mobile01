1b32b328dbb418aaee419fc14af9dbee0da4d6d4
Maurolima <shamauro.ml@gmail.com>
2022-06-29 21:46:32 -0300

    primeiro

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.modules.css'
+
+function Footer() {
+  return <footer >
+    <div className={styles.rodape}>
+    <p >Rodapé</p>
+    </div>
+    </footer>
+}
+
+export default Footer
